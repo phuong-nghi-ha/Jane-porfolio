fc43522d288e7f9c7a77bb9e8c314cbcc390fb9d
phuong-nghi-ha <165134277+phuong-nghi-ha@users.noreply.github.com>
2024-04-09 14:08:30 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["Welcome to my Portfolio"],
+       	 	  typeSpeed:60,
+        		backSpeed:50,
+        		loop:true,
+        		});
+new WOW().init();
+
+ 
